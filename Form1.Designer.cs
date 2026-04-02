b46321eb04ddaafa2c58ef173dacc7793657446b
qwe-r-r <sohyun031031@gmail.com>
2026-04-02 14:55:55 +0900

    초기 IU 배치 및 메서드 이름 수정

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace LoginScreen
 {
-    partial class Form1
+    partial class LoginScreen
     {
         /// <summary>
         ///  Required designer variable.
@@ -28,18 +28,68 @@
         /// </summary>
         private void InitializeComponent()
         {
+            Loginlbl = new Label();
+            IDtbox = new TextBox();
+            PWxtbox = new TextBox();
+            logInbtn = new Button();
             SuspendLayout();
             // 
-            // Form1
+            // Loginlbl
+            // 
+            Loginlbl.AutoSize = true;
+            Loginlbl.Font = new Font("맑은 고딕", 32F);
+            Loginlbl.Location = new Point(120, 61);
+            Loginlbl.Name = "Loginlbl";
+            Loginlbl.Size = new Size(134, 59);
+            Loginlbl.TabIndex = 0;
+            Loginlbl.Text = "Login";
+            // 
+            // IDtbox
+            // 
+            IDtbox.Location = new Point(95, 180);
+            IDtbox.Multiline = true;
+            IDtbox.Name = "IDtbox";
+            IDtbox.Size = new Size(200, 50);
+            IDtbox.TabIndex = 1;
+            // 
+            // PWxtbox
+            // 
+            PWxtbox.Location = new Point(95, 236);
+            PWxtbox.Multiline = true;
+            PWxtbox.Name = "PWxtbox";
+            PWxtbox.Size = new Size(200, 50);
+            PWxtbox.TabIndex = 2;
+            // 
+            // logInbtn
+            // 
+            logInbtn.Font = new Font("맑은 고딕", 12F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            logInbtn.Location = new Point(120, 319);
+            logInbtn.Name = "logInbtn";
+            logInbtn.Size = new Size(150, 50);
+            logInbtn.TabIndex = 3;
+            logInbtn.Text = "로그인";
+            logInbtn.UseVisualStyleBackColor = true;
+            // 
+            // LoginScreen
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(409, 554);
-            Name = "Form1";
-            Text = "Form1";
+            ClientSize = new Size(384, 561);
+            Controls.Add(logInbtn);
+            Controls.Add(PWxtbox);
+            Controls.Add(IDtbox);
+            Controls.Add(Loginlbl);
+            Name = "LoginScreen";
+            Text = "LoginScreen";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label Loginlbl;
+        private TextBox IDtbox;
+        private TextBox PWxtbox;
+        private Button logInbtn;
     }
 }
